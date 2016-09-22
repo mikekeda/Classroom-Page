2fe5c66809fd93f20581d1221ec2fbc68d1ff9e6
Devendra Vyas <devendra.y12@lnmiit.ac.in>
2016-09-22 15:02:52 +0530

    TO DO change in settings.py for shifting from sqlite to PostGreSQL

--- ClassroomPage/settings.py
+++ ClassroomPage/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'ClassroomPage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+#Database : PostGreSql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dn',
+        'USER': 'dnadmin',
+        'PASSWORD': 'dnadmin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
